a53dddac6377e244d13afb2a4ff88b39b1bad9aa
nandas <nandakumararaja@gmail.com>
2020-10-08 19:12:06 -0400

    New search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import Accordion from './components/Accordion';
+import Search from './components/Search';
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a front end javascript framework',
-  },
-  {
-    title: 'Why use React?',
-    content: 'React is a favorite JS library among engineers',
-  },
-  {
-    title: 'How do you use React?',
-    content: 'You use React by creating components',
-  },
-];
 
 export default () => {
   return (
     <div>
-      <Accordion items={items} />
+      {/*<Accordion items={items} />*/}
+      <Search/>
     </div>
   );
 };
