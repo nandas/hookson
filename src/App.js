f46d3041da045aaf3c317308c3ef523eb3d19747
nandas <nandakumararaja@gmail.com>
2020-10-09 23:52:32 -0400

    removed  items

--- src/App.js
+++ src/App.js
@@ -3,21 +3,6 @@ import Dropdown from './components/Dropdown';
 
 
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a front end javascript framework',
-  },
-  {
-    title: 'Why use React?',
-    content: 'React is a favorite JS library among engineers',
-  },
-  {
-    title: 'How do you use React?',
-    content: 'You use React by creating components',
-  },
-];
-
 
 const options = [
   {
